4c6f5a78c7e4192f61f1ef883824670fbdbaaf13
unknown <futemaaaa@gmail.com>
2023-04-13 15:31:57 -0300

    No exercício 9 fiz um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.

--- exercício09.py
+++ exercício09.py
@@ -2,4 +2,13 @@ A = []
 
 for i in range (10):
     numero = int(input(f"Digite o {i+1}º numero:"))
-    
+    A.append(numero)
+
+soma = sum(A)
+multiplicação = 1
+for numero in A:
+    multiplicação *= numero
+print ("soma dos numeros:")
+print (soma)
+print ("numero da soma ao quadrado:")
+print (multiplicação)
